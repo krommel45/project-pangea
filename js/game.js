437232944e2f122121273cca297d30d18a3b166b
krommel45 <krommel45@gmail.com>
2018-08-28 20:37:25 -0400

    add more images

--- js/game.js
+++ js/game.js
@@ -17,11 +17,17 @@ gameScene.preload = function() {
  
   // load images
   this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png')
+  this.load.image('dragon', 'assets/dragon.png')
+  this.load.image('treasure', 'assets/treasure.png')
 };
  
 // executed once, after assets were loaded
 gameScene.create = function() {
  
    // background
-   this.add.sprite(0, 0, 'background');
-}
+   let bg = this.add.sprite(0, 0, 'background');
+
+   // change origin to the top-left of the sprite
+  bg.setOrigin(0,0);
+};
